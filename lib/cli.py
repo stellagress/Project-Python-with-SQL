69793e603611073337c0b96cd58d3bdaa738227b
Stella Mazzillo <stellagress5@gmail.com>
2023-08-24 01:48:56 -0400

    cli.py works

---
+++ lib/cli.py
@@ -0,0 +1,31 @@
+from simple_term_menu import TerminalMenu
+
+
+
+def start():
+    print("     _       _           _            _")
+    print("    / \\   __| |_ __ ___ (_)_ __      / \\   ___ ___ ___  ___ ___")
+    print("   / _ \\ / _` | '_ ` _ \\| | '_ \\    / _ \\ / __/ __/ _ \\/ __/ __|")
+    print("  / ___ \\ (_| | | | | | | | | | |  / ___ \\ (_| (_|  __/\\__ \\__ \\")
+    print(" /_/   \\_\\__,_|_| |_| |_|_|_| |_| /_/   \\_\\___\\___\\___||___/___/")
+
+    # for _ in range(3):
+    print("\n")
+
+
+
+
+
+
+    print("1. View Current Inventory")
+    print("2. Update Inventory")
+    print("3. SOS order")
+    print("4. Exit")
+    user_input = input("Please make a selection (1-3): ")
+
+ 
+
+start()
+
+
+

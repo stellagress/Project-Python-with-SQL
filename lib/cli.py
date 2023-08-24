2dbcd53c0c4ce9c8cc509a6168b07c2af14e2a32
Stella Mazzillo <stellagress5@gmail.com>
2023-08-24 01:59:44 -0400

    adds simple-term-menu

--- lib/cli.py
+++ lib/cli.py
@@ -12,16 +12,26 @@ def start():
     # for _ in range(3):
     print("\n")
 
+    print("-----------------MARILYN'S NAIL SALON RESTRICTED PORTAL-----------------")
+
+    print("\n")
+
+    options = ["1. View Current Inventory", "2. Update Inventory", "3. SOS order", "4. Exit"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry = terminal_menu.show()
+    print(f"Confirm selection: {options[menu_entry]}")
 
 
 
 
 
-    print("1. View Current Inventory")
-    print("2. Update Inventory")
-    print("3. SOS order")
-    print("4. Exit")
-    user_input = input("Please make a selection (1-3): ")
+
+
+    # print("1. View Current Inventory")
+    # print("2. Update Inventory")
+    # print("3. SOS order")
+    # print("4. Exit")
+    # user_input = input("Please make a selection (1-3): ")
 
  
 
